29b7195c6779aab99a735d9dddfb47499ee9156c
Jonti <jontisavani+kraken@gmail.com>
2020-07-13 17:27:06 +0530

    - added the medium-editor

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <editor />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Editor from "./components/AppEditor.vue";
+
+@Component({
+  components: {
+    Editor
+  }
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/AppEditor.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="editor">
+    <h4>This is an feature rich editor!!</h4>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import MediumEditor from "medium-editor/dist/js/medium-editor.min";
+import "medium-editor/dist/css/medium-editor.min.css";
+import "medium-editor/dist/css/themes/default.min.css";
+
+@Component
+export default class AppEditor extends Vue {
+  editor = null;
+
+  mounted() {
+    this.editor = new MediumEditor(".editor");
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+[contenteditable]:focus {
+  outline: 0 solid transparent;
+}
+
+.editor {
+  border: none;
+  margin: 20px;
+  padding: 20px;
+  background-color: whitesmoke;
+}
+</style>
